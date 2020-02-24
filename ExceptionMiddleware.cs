5886fa78107809aebfee9c390569424532faaac6
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-02-24 12:40:00 -0500

    directly map strings

--- ExceptionMiddleware.cs
+++ ExceptionMiddleware.cs
@@ -17,11 +17,14 @@ namespace SgtMapper {
       } catch (Exception e) {
         var mappedResponse = SgtMapper.MapException(e);
 
+        if (mappedResponse is string) {
+          await context.Response.WriteAsync(mappedResponse.ToString());
+        }
         try {
           var serialized = JsonConvert.SerializeObject(mappedResponse);
           await context.Response.WriteAsync(serialized);
         } catch {
-          await context.Response.WriteAsync(mappedResponse.ToString());
+          await context.Response.WriteAsync("An error occurred while mapping the exception. Make sure to return a string value or a JSON.NET serializable object.");
         }
       }
     }
