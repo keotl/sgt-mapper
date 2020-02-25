f9977306adf339cfff6299d94f3782aeea82791b
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-02-25 18:18:02 -0500

    do catch all exceptions

--- SgtMapper.cs
+++ SgtMapper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 
 namespace SgtMapper {
 
@@ -9,8 +10,10 @@ namespace SgtMapper {
 
     private static Dictionary<Type, Type> exceptionMappers = new Dictionary<Type, Type>();
     private static IServiceProvider serviceProvider;
+    private static ILogger<ExceptionMiddleware> logger = new Logger<ExceptionMiddleware>(new LoggerFactory());
+    private static bool silent;
 
-    public static void WithSgtMapper(this IApplicationBuilder app) {
+    public static void WithSgtMapper(this IApplicationBuilder app, bool suppressUnknownExceptionTypeLogging = false) {
       serviceProvider = app.ApplicationServices;
       exceptionMappers = AppDomain.CurrentDomain
           .GetAssemblies()
@@ -18,14 +21,19 @@ namespace SgtMapper {
           .Where(t => t.GetCustomAttributes(typeof(ExceptionMapperAttribute), true).Length > 0)
           .Where(t => t.GetInterfaces()[0].GetGenericArguments().Length > 0) // TODO use correct interface, not necessarily first one
           .ToDictionary(t => t.GetInterfaces()[0].GetGenericArguments()[0], t => t);
-
+      silent = !suppressUnknownExceptionTypeLogging;
       app.UseMiddleware<ExceptionMiddleware>();
     }
 
     public static object MapException(Exception e) {
 
       if (!exceptionMappers.TryGetValue(e.GetType(), out var mapperType)) {
-        throw new Exception($"Could not find the an exception mapper for type '{e.GetType()}'. Make sure your exception mappers are correctly annotated.");
+        if (!silent) {
+          logger.LogInformation($"SgtMapper: Could not find an exception mapper for '${e.GetType()}'. " +
+                                "If this is not intended, make sure that your exception types are properly annotated. " +
+                                "To suppress this message, configure SgtMapper using 'WithSgtMapper(true)'.");
+        }
+        throw e;
       }
 
       var mapper = serviceProvider.GetService(mapperType);
@@ -38,7 +46,7 @@ namespace SgtMapper {
       }
 
       var methodInfo = mapperType.GetMethod("Map");
-      return methodInfo.Invoke(mapper, new[]{e});
+      return methodInfo.Invoke(mapper, new[] { e });
     }
   }
 }
