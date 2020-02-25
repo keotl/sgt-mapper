ddbba0a28eba3fb96338e57ecb9decb4b4f632ea
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-02-25 18:23:16 -0500

    fix exception serialization

--- ExceptionMiddleware.cs
+++ ExceptionMiddleware.cs
@@ -19,12 +19,14 @@ namespace SgtMapper {
 
         if (mappedResponse is string) {
           await context.Response.WriteAsync(mappedResponse.ToString());
-        }
-        try {
-          var serialized = JsonConvert.SerializeObject(mappedResponse);
-          await context.Response.WriteAsync(serialized);
-        } catch {
-          await context.Response.WriteAsync("An error occurred while mapping the exception. Make sure to return a string value or a JSON.NET serializable object.");
+        } else {
+          try {
+            var serialized = JsonConvert.SerializeObject(mappedResponse);
+            context.Response.Headers.Add("Content-Type", "application/json");
+            await context.Response.WriteAsync(serialized);
+          } catch {
+            await context.Response.WriteAsync("An error occurred while mapping the exception. Make sure to return a string value or a JSON.NET serializable object.");
+          }
         }
       }
     }
